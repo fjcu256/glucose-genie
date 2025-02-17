5ce10504c27284f1105aaac03b6d4334d5979b5d
Kristina Hristova <42977135+khristova21@users.noreply.github.com>
2025-02-17 14:40:55 -0500

    Add GlucoseGenie project (#9)

---
+++ src/application/GlucoseGenie/GlucoseGenieUITests/GlucoseGenieUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GlucoseGenieUITestsLaunchTests.swift
+//  GlucoseGenieUITests
+//
+//  Created by Hristova,Krisi on 2/17/25.
+//
+
+import XCTest
+
+final class GlucoseGenieUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
