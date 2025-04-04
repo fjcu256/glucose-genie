8836706282e214f619e1aefc7c3fe073531fea16
Krisi Hristova <hristovakrisi@gmail.com>
2025-04-04 03:34:44 -0400

    Create new Recipe structure, add API from Edamam, parse and display recipes in RecipeUIView

---
+++ src/application/GlucoseGenie/GlucoseGenie/Recipes/Networking/RecipeParser.swift
@@ -0,0 +1,71 @@
+//
+//  RecipeParser.swift
+//  GlucoseGenie
+//
+//  Created by Hristova,Krisi on 4/3/25.
+//
+
+import Foundation
+
+struct RecipeParser {
+    static func parseRecipes(from data: Data) -> [Recipe] {
+        do {
+            let decodedResponse = try JSONDecoder().decode(RecipeAPIResponse.self, from: data)
+            
+            let recipes: [Recipe] = decodedResponse.hits.map {hit in
+                let edamam = hit.recipe
+                return Recipe(
+                    name: edamam.label,
+                    image: edamam.image,
+                    ingredients: edamam.ingredients.map { Ingredient(text: $0.text, quantity: $0.quantity, units: $0.measure ?? "") },
+                    totalNutrients: parseTotalNutrients( from: edamam.totalNutrients),
+                    diets: edamam.dietLabels?.compactMap {DietType(rawValue: normalizeDietLabel($0)) } ?? [],
+                    mealtypes: edamam.mealType?.compactMap {MealType(rawValue: normalizeMealType($0)) } ?? [],
+                    healthLabels: edamam.healthLabels?.compactMap {HealthLabel(rawValue: normalizeHealthLabel($0)) } ?? []
+                )
+            }
+            return recipes
+        } catch {
+            print("JSON Parsing into Recipes Failed: \(error)")
+            return []
+        }
+    }
+    
+    // Extracts needed parameters for each nutrient.
+    // Creates a Nutrient and stores it in a dictionary.
+    // Returns dictionary of Nutrients.
+    private static func parseTotalNutrients(from dict: [String: NutrientAPI]?) -> [Nutrient] {
+        guard let dict = dict else {return []}
+        
+        return dict.map {
+            (key, value) in Nutrient(name: value.label, quantity: value.quantity, unit: value.unit)
+        }
+    }
+    
+    private static func normalizeDietLabel(_ rawLabel: String) -> String {
+        // ex. "Low-Carb" -> "lowCarb"
+        return rawLabel.replacingOccurrences(of: "-", with: "").lowercased()
+    }
+    
+    private static func normalizeMealType(_ rawMeal: String) -> String {
+        let meal = rawMeal.lowercased()
+        if meal.contains("lunch") {
+            return "lunch"
+        } else if meal.contains("dinner"){
+            return "dinner"
+        } else if meal.contains("breakfast"){
+            return "breakfast"
+        } else if meal.contains("snack") {
+            return "snack"
+        }
+        return meal
+    }
+    
+    private static func normalizeHealthLabel(_ rawLabel: String) -> String {
+        return rawLabel
+            .replacingOccurrences(of: "-", with: "")
+            .replacingOccurrences(of: " ", with: "")
+            .lowercased()
+    }
+    
+}
--- src/application/GlucoseGenie/GlucoseGenie/Recipes/View/RecipeUIView.swift
+++ src/application/GlucoseGenie/GlucoseGenie/Recipes/View/RecipeUIView.swift
@@ -7,18 +7,30 @@
 
 import SwiftUI
 
-struct Recipe: Identifiable, Equatable, Codable {
+/*struct Recipe: Identifiable, Equatable, Codable {
     let id = UUID()
     let name: String
     let imageUrl: String
     let calories: Int
     let carbs: Int
-}
+    
+}*/
 struct RecipeUIView: View {
+    @State private var allRecipes: [Recipe] = []
+    @State private var searchQuery: String = ""
+    @State private var selectedFilters: Set<String> = []
+    @State private var isLoading: Bool = true
+    
+    let filters: [String] = ["vegetarian", "low-carb", "breakfast", "lunch", "dinner", "snack"]
+    
+    // TODO
+    //var filteredRecipes: [Recipe] {    }
+    
+    
     @State private var likedRecipes: [Recipe] = []
     
     // FIXME Hard Coded Recipe info and URLs
-    let recipes: [Recipe] = [
+    /*let recipes: [Recipe] = [
         Recipe(name: "Grilled Veggie Wrap",
                imageUrl: "https://diabetesfoodhub.org/sites/foodhub/files/styles/375_375_2x/public/shutterstock_425424460.webp?h=e2d4e250&itok=ptCQ_FGY",
                calories: 110,
@@ -39,14 +51,10 @@ struct RecipeUIView: View {
                imageUrl: "https://diabetesfoodhub.org/sites/foodhub/files/styles/recipe_hero_banner_720w_2x/public/2019-diabetic-breakfast-zucchini-egg-boat_diabetes-cookbook_081618_1021x779.webp?h=48784f2c&itok=clFreezC",
               calories: 160,
                carbs: 8),
-    ]
-    
-    @State private var searchQuery: String = ""
-    @State private var selectedFilters: Set<String> = []
-    let filters: [String] = ["vegetarian", "low carb", "chinese"]
+    ]*/
     
     var filteredRecipes: [Recipe] {
-        var result = recipes
+        var result = allRecipes
         
         // Search
         // TODO Implement search results through API search.
@@ -55,8 +63,8 @@ struct RecipeUIView: View {
         }
         
         // Filters
-        // TODO Implement filtering through API
-        if !selectedFilters.isEmpty {
+        // TODO Implement filtering through API for mealtype, diet type, etc.
+        /*if !selectedFilters.isEmpty {
             result = result.filter { recipe in
                 selectedFilters.allSatisfy { filter in
                     switch filter {
@@ -70,7 +78,7 @@ struct RecipeUIView: View {
                 }
                 
             }
-        }
+        }*/
         return result
     }
     
@@ -129,46 +137,73 @@ struct RecipeUIView: View {
                 }
                 
                 // Scrollable Grid Layout of Recipes.
-                LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 16) {
-                    ForEach(recipes) { recipe in
-                        VStack {
-                            AsyncImage(url: URL(string: recipe.imageUrl)) { image in
-                                image.resizable()
-                                    .scaledToFit()
-                                    .frame(width: 150, height: 150)
-                                    .clipShape(RoundedRectangle(cornerRadius: 12))
-                            } placeholder: {
-                                ProgressView()
-                            }
-                            
-                            VStack(spacing: 4) {
-                                Text(recipe.name)
-                                    .font(.headline)
-                                    .foregroundColor(.primary)
-                                    .multilineTextAlignment(.center)
+                if isLoading {
+                    Spacer()
+                    ProgressView()
+                    Spacer()
+                } else {
+                    LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 16) {
+                        ForEach(allRecipes) { recipe in
+                            VStack {
+                                // Show Image or place holder.
+                                if let imageUrl = recipe.imageUrl {
+                                    AsyncImage(url: imageUrl) { phase in
+                                        if let image = phase.image {
+                                            image.resizable()
+                                                .scaledToFit()
+                                                .frame(width: 150, height: 150)
+                                                .clipShape(RoundedRectangle(cornerRadius: 12))
+                                        } else if phase.error != nil {
+                                            placeHolderEmoji
+                                        } else {
+                                            ProgressView().frame(width: 150, height: 150)
+                                        }
+                                    }
+                                } else {
+                                    placeHolderEmoji
+                                }
                                 
-                                Text("Calories: \(recipe.calories)  Carbs: \(recipe.carbs)g")
+                                VStack(spacing: 4) {
+                                    Text(recipe.name)
+                                        .font(.headline)
+                                        .foregroundColor(.primary)
+                                        .multilineTextAlignment(.center)
+                                    
+                                    // Show the calorie and carb info if available.
+                                    HStack {
+                                        if let calories = recipe.calories {
+                                            Text("Calories: \(calories) kcal")
+                                        }
+                                        if let carbs = recipe.carbs {
+                                            Text("Carbs: \(carbs)g")
+                                        }
+                                    }
                                     .font(.subheadline)
                                     .foregroundColor(.secondary)
                                     .multilineTextAlignment(.center)
-                                
-                                Button(action: { toggleLike(recipe)}) {
-                                    Image(systemName: likedRecipes.contains(recipe) ? "heart.fill" : "heart")
-                                        .foregroundColor(.red)
-                                        .padding(.top, 4)
+                                    Button(action: { toggleLike(recipe)}) {
+                                        Image(systemName: likedRecipes.contains(recipe) ? "heart.fill" : "heart")
+                                            .foregroundColor(.red)
+                                            .padding(.top, 4)
+                                    }
                                 }
-                                
+                                .frame(maxWidth: .infinity)
+                                .multilineTextAlignment(.center)
+                                .padding(.top, 4)
                             }
-                            .frame(maxWidth: .infinity)
-                            .multilineTextAlignment(.center)
-                            .padding(.top, 4)
+                            .padding()
+                            .background(RoundedRectangle(cornerRadius: 12).fill(Color(.systemGray6)))
                         }
-                        .padding()
-                        .background(RoundedRectangle(cornerRadius: 12).fill(Color(.systemGray6)))
-                    }
-                }.padding()
+                    }.padding()
+                }
             }.navigationTitle("Recipes")
         }
+        .onAppear {
+            if allRecipes.isEmpty{
+                fetchInitialRecipes()
+            }
+        }
+        
     }
     
     func toggleLike(_ recipe: Recipe) {
@@ -185,6 +220,63 @@ struct RecipeUIView: View {
             // API call to save to DB.
             print("Saved Recipe: \(recipe.name)")
         }
+    
+    
+    func fetchInitialRecipes() {
+        let baseUrl = "https://api.edamam.com/api/recipes/v2"
+        
+        var query = URLComponents(string: baseUrl)!
+        query.queryItems = [
+            URLQueryItem(name: "type", value: "public"),
+            URLQueryItem(name: "app_id", value: Secrets.appId),
+            URLQueryItem(name: "app_key", value: Secrets.appKey),
+            URLQueryItem(name: "glycemicIndex", value: "0.0-69.0")
+        ]
+        
+        // Setup GET Request in English.
+        var request = URLRequest(url: query.url!)
+        request.httpMethod = "GET"
+        request.addValue("application/json", forHTTPHeaderField: "accept")
+        request.addValue("en", forHTTPHeaderField: "Accept-Language")
+        
+        // Response
+        let task = URLSession.shared.dataTask(with: request) {data, response, error in
+            if let error = error {
+                print("Request Error: \(error.localizedDescription)")
+                return
+            }
+            guard let httpResponse = response as? HTTPURLResponse else {
+                print("No valid response")
+                return
+            }
+            
+            print("Status Code: \(httpResponse.statusCode)")
+            guard let data = data else {
+                print("No data received")
+                return
+            }
+            let parsedRecipes = RecipeParser.parseRecipes(from: data)
+            
+            // DEBUGGING
+            if let json = String(data: data, encoding: .utf8) {
+                print("JSON Response: \(json)")
+            }
+            
+            DispatchQueue.main.async {
+                self.allRecipes = parsedRecipes
+                self.isLoading = false
+            }
+        }
+        task.resume()
+    }
+    
+    private var placeHolderEmoji: some View {
+        Text("🍽️")
+            .font(.system(size: 60))
+            .frame(width: 150, height: 150)
+            .background(Color.gray.opacity(0.2))
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+    }
 }
 
 #Preview {
