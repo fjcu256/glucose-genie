4127f652b9e489c9e209121daba99994726d7608
Krisi Hristova <hristovakrisi@gmail.com>
2025-04-05 21:47:01 -0400

    Add badge to recipe search, grocery list, saved recipes pages

--- src/application/GlucoseGenie/GlucoseGenie/Settings/View/SavedRecipesView.swift
+++ src/application/GlucoseGenie/GlucoseGenie/Settings/View/SavedRecipesView.swift
@@ -10,6 +10,12 @@ import SwiftUI
 struct SavedRecipesView: View {
     var body: some View {
         Text("Saved Recipes")
+        Spacer(minLength: 30)
+        Image("EdamamBadge")
+            .resizable()
+            .scaledToFit()
+            .frame(height: 30)
+            .padding(.bottom, 20)
     }
 }
 
