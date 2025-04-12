e160ff89403f849830bd29b600c23979efb45e9f
JJackson914 <jmj379@drexel.edu>
2025-04-12 15:24:17 -0400

    Initial weekly meal planner commit

--- src/application/GlucoseGenie/GlucoseGenie/Settings/View/WeeklyMealPlanView.swift
+++ src/application/GlucoseGenie/GlucoseGenie/Settings/View/WeeklyMealPlanView.swift
@@ -9,9 +9,25 @@ import SwiftUI
 
 struct WeeklyMealPlanView: View {
     var body: some View {
-        Text("Weekly Meal Plan View")
+        // Text("Weekly Meal Plan View")
         // Weekly meal plan page should have a section that
         // shows nutrient totals from recipes.
+        NavigationView {
+            ScrollView {
+                VStack(spacing: 20) {
+                    Text("Test")
+                }
+                .padding()
+            }
+            .navigationTitle("Weekly Meal Planner")
+            .toolbar {
+                Button(action: {
+                    // Action for adding or editing meals
+                }) {
+                    Image(systemName: "square.and.pencil")
+                }
+            }
+        }
     }
 }
 
