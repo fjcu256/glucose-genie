bd58ee2df6f5a15b65a0ba7056d640d98781c319
Carson Ford <carsondrex@gmail.com>
2025-05-12 17:02:18 -0400

    Automated 003

--- src/application/GlucoseGenie/GlucoseGenieUITests/GroceryStoreMapUITests.swift
+++ src/application/GlucoseGenie/GlucoseGenieUITests/GroceryStoreMapUITests.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Ford,Carson on 5/12/25.
 //
+// Add any UI test cases here that validate any feature of the Grocery Store Map ("Find a Store") page.
 
 import XCTest
 
@@ -25,7 +26,7 @@ final class GroceryStoreMapUITests: XCTestCase {
 
         // Perform a pinch out to zoom in
         map.pinch(withScale: 2.0, velocity: 1.0)
-        sleep(1) // Wait for zoom animation to finish
+        sleep(1)
 
         // Perform a pinch in to zoom out
         map.pinch(withScale: 0.5, velocity: -1.0)
@@ -34,5 +35,25 @@ final class GroceryStoreMapUITests: XCTestCase {
         // MapKit does not expose zoom level directly to UI tests.
         // So this test mainly ensures gestures are recognized and don't crash.
     }
+    
+    func testMapIsPannable() {
+        let map = app.otherElements["MainMap"]
+        XCTAssertTrue(map.waitForExistence(timeout: 5), "Map should exist")
+
+        // Swipe gestures to simulate panning
+        map.swipeUp()
+        sleep(1)
+
+        map.swipeDown()
+        sleep(1)
+
+        map.swipeLeft()
+        sleep(1)
+
+        map.swipeRight()
+        sleep(1)
+        
+        // If gestures do not crash, then we have validated map-panning functionality
+        }
 
 }
